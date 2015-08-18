63933ed112e2ad59a914595f6b446e3a8ab30f06
Michael Klich <michal@michalklich.com>
2015-08-18 22:42:32 +0100

    Define command line args

--- bumper.hs
+++ bumper.hs
@@ -3,15 +3,15 @@
 
 module Main (main) where
 
+import System.IO
 import System.Console.CmdArgs
 
-data Bumper = Bumper {current_version :: String} deriving (Show, Data, Typeable)
+data Bumper = Bumper {current_version :: String, part :: String, file :: String}
+              deriving (Show, Data, Typeable)
 
 bumper = Bumper {
-         current_version = def &= help "The current version of the software package before bumping."
-         }
+         current_version = def &= help "The current version of the software package before bumping." &= typ "VERSION",
+         part = def &= argPos 0 &= typ "PART",
+         file = def &= argPos 1 &= typFile}
          &= help ("Version bumping software.")
          &= summary "bumper v0.0.1, Michał Klich"
-
-
-main = cmdArgs bumper
