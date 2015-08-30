e289ea1b43af77d811afaa46c80a9b98851ddc4e
Michael Klich <michal@michalklich.com>
2015-08-30 22:42:40 +0100

    Remove some bad code

--- src/Bumper.hs
+++ src/Bumper.hs
@@ -43,14 +43,6 @@ makeVersion chunks = intercalate "." chunks
 versionBumper :: [Char] -> [[Char]] -> [Char]
 versionBumper part current = makeVersion current
 
-    -- | current == [] = ""
-    -- -- wider pattern match, this can catch all parts
-    -- | part == "major" = major ++ minor ++ patch
-    -- | part == "minor" = show (read current + 1) :: [Char]
-    -- | part == "patch" = major ++ minor ++ patch
-    -- | otherwise = major ++ minor ++ patch
-    -- where (major:minor:patch) = current
-
 exec :: Bumper -> IO ()
 exec Bumper{..} = do
   contents <- readFile file
